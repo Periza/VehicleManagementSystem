9b60dfdb26a707b5d0f580dcff54df7d3f980d77
Marko Perica <marko.perica2@gmail.com>
2024-05-25 05:06:17 +0200

    Use in memory database, map scaffolded Identity Razor pages.

--- VehicleManagementSystem/Program.cs
+++ VehicleManagementSystem/Program.cs
@@ -1,9 +1,24 @@
 using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Project.Service.Data;
 
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Services.AddDbContext<ApplicationDbContext>(optionsAction: options =>
+{
+    options.UseInMemoryDatabase(databaseName: "InMemoryDb");
+});
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,12 +33,22 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    context.Database.EnsureCreated(); // This will trigger OnModelCreating
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
