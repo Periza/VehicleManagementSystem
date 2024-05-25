d1cc882e9586dbb0d2aca936fac5935d371ef8c9
Marko Perica <marko.perica2@gmail.com>
2024-05-25 13:40:56 +0200

    Set subtype to html

---
+++ VehicleManagementSystem/EmailService.cs
@@ -0,0 +1,40 @@
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.QuickInfo;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using MimeKit.Text;
+using VehicleManagementSystem.Service.SettingModels;
+
+namespace Project.Service.Data;
+
+public class EmailService(IOptions<MailSettings> mailSettings) : IEmailSender
+{
+
+    private readonly MailSettings _mailSettings = mailSettings.Value;
+
+    public async  Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        MimeMessage message = new();
+        message.From.Add(new MailboxAddress(name: "admin", address: _mailSettings.FromAddress));
+        message.To.Add(new MailboxAddress(name: "customer", address: "customer@example.com"));
+        message.Subject = subject;
+
+        message.Body = new TextPart(subtype: "html")
+        {
+            Text = htmlMessage
+        };
+
+        using (var client = new SmtpClient())
+        {
+            await client.ConnectAsync(host: _mailSettings.Host, _mailSettings.Port);
+            
+            // Sent the email
+            await client.SendAsync(message: message);
+            
+            // Disconnect
+            await client.DisconnectAsync(quit: true);
+        }
+    }
+}
