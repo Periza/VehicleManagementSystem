9346e392e35c66858d92988c973dd8fac47c1025
Marko Perica <marko.perica2@gmail.com>
2024-05-27 16:08:45 +0200

    Add implementation of VehicleRepository

---
+++ VehicleManagementSystem.Service/Repositories/VehicleRepository.cs
@@ -0,0 +1,81 @@
+using Microsoft.EntityFrameworkCore;
+using Project.Service.Data;
+using VehicleManagementSystem.Service.Models;
+
+namespace VehicleManagementSystem.Service.Repositories;
+
+public class VehicleRepository : IVehicleRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public VehicleRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public async Task<IEnumerable<VehicleMake>> GetAllMakesAsync()
+    {
+        return await _dbContext.VehicleMakes.ToListAsync();
+    }
+
+    public async Task<Optional<VehicleMake>> GetMakeByIdAsync(int id)
+    {
+        VehicleMake? make = await _dbContext.VehicleMakes.FindAsync(keyValues: id);
+        return make is not null ? Optional<VehicleMake>.Of(value: make) : Optional<VehicleMake>.Empty();
+    }
+
+    public async Task AddMakeAsync(VehicleMake make)
+    {
+        _dbContext.VehicleMakes.Add(entity: make);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateMakeAsync(VehicleMake make)
+    {
+        _dbContext.VehicleMakes.Update(entity: make);
+        await _dbContext.SaveChangesAsync();
+    }
+    
+
+    public async Task DeleteMakeAsync(int id)
+    {
+        VehicleMake? make = await _dbContext.VehicleMakes.FindAsync(keyValues: id);
+        if (make is not null)
+        {
+            _dbContext.VehicleMakes.Remove(entity: make);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
+    public async Task<IEnumerable<VehicleModel>> GetAllModelsAsync()
+    {
+        return await _dbContext.VehicleModels.ToListAsync();
+    }
+
+    public async Task<Optional<VehicleModel>> GetModelByIdAsync(int id)
+    {
+        VehicleModel? model = await _dbContext.VehicleModels.FindAsync(id);
+        return model is not null ? Optional<VehicleModel>.Of(model) : Optional<VehicleModel>.Empty();
+    }
+
+    public async Task<IEnumerable<VehicleModel>> GetModelsByMakeIdAsync(int makeId)
+    {
+        return await _dbContext.VehicleModels.Where(m => m.MakeId == makeId).ToListAsync();
+    }
+
+    public async Task<IEnumerable<VehicleModel>> GetModelsAsync(string searchTerm, string sortBy, bool ascending, int pageNumber, int pageSize)
+    {
+        IQueryable<VehicleModel> query = _dbContext.VehicleModels.AsQueryable();
+
+        if (!string.IsNullOrEmpty(value: searchTerm))
+            query = query.Where(m => m.Name.Contains(searchTerm));
+
+        query = sortBy switch
+        {
+            "Name" => ascending ? query.OrderBy(m => m.Name) : query.OrderByDescending(m => m.Name),
+            _ => query
+        };
+
+        return await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+    }
+}
