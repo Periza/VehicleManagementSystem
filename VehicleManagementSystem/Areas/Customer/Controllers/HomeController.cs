1992f2106c1a5a5d2ac414e4007fcfcb2bae4f11
Marko Perica <marko.perica2@gmail.com>
2024-05-27 21:30:15 +0200

    Add action for all vehicles page

--- VehicleManagementSystem/Areas/Customer/Controllers/HomeController.cs
+++ VehicleManagementSystem/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using VehicleManagementSystem.Models;
+using VehicleManagementSystem.Service.Services.Vehicle;
+using VehicleManagementSystem.Service.ViewModels;
 
 namespace VehicleManagementSystem.Controllers
 {
@@ -8,10 +10,12 @@ namespace VehicleManagementSystem.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IVehicleService _vehicleService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IVehicleService vehicleService)
         {
             _logger = logger;
+            _vehicleService = vehicleService;
         }
 
         public IActionResult Index()
@@ -22,6 +26,12 @@ namespace VehicleManagementSystem.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Vehicles()
+        {
+            IEnumerable<VehicleModelViewModel> vehicles = await _vehicleService.GetAllModelsAsync();
+            return View(vehicles);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
