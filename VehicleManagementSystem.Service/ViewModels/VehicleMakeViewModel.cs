6c4ef5662c9dfc0f934d0caa30f2bb85bbd7b47e
Marko Perica <marko.perica2@gmail.com>
2024-05-27 16:08:09 +0200

    Add VehicleMake and Model View Models

---
+++ VehicleManagementSystem.Service/ViewModels/VehicleMakeViewModel.cs
@@ -0,0 +1,8 @@
+namespace VehicleManagementSystem.Service.ViewModels;
+
+public class VehicleMakeViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Abrv { get; set; } = string.Empty;
+}
---
+++ VehicleManagementSystem.Service/ViewModels/VehicleModelViewModel.cs
@@ -0,0 +1,9 @@
+namespace VehicleManagementSystem.Service.ViewModels;
+
+public class VehicleModelViewModel
+{
+    public int Id { get; set; }
+    public int MakeId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Abrv { get; set; } = string.Empty;
+}
