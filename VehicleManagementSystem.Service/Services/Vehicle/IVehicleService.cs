d27900b88028f15e4a64c477dba59f374fdca9a1
Marko Perica <marko.perica2@gmail.com>
2024-05-27 17:38:36 +0200

    Add VehicleService

--- VehicleManagementSystem.Service/Services/Vehicle/IVehicleService.cs
+++ VehicleManagementSystem.Service/Services/Vehicle/IVehicleService.cs
@@ -5,24 +5,13 @@ namespace VehicleManagementSystem.Service.Services.Vehicle;
 
 public interface IVehicleService
 {
-    // Methods for Vehicle Make
     Task<IEnumerable<VehicleMakeViewModel>> GetAllMakesAsync();
-    Task<VehicleMakeViewModel> GetMakeByIdAsync(int id);
+    Task<Optional<VehicleMakeViewModel>> GetMakeByIdAsync(int id);
     Task AddMakeAsync(VehicleMakeViewModel makeViewModel);
     Task UpdateMakeAsync(VehicleMakeViewModel makeViewModel);
     Task DeleteMakeAsync(int id);
-
-    // Methods for Vehicle Model
     Task<IEnumerable<VehicleModelViewModel>> GetAllModelsAsync();
+    Task<Optional<VehicleModelViewModel>> GetModelByIdAsync(int id);
     Task<IEnumerable<VehicleModelViewModel>> GetModelsByMakeIdAsync(int makeId);
-
-    // Sorting Method
-    IEnumerable<VehicleModelViewModel> SortModels(IEnumerable<VehicleModelViewModel> models, string sortBy, bool ascending);
-
-    // Filtering Method
-    IEnumerable<VehicleModelViewModel> FilterModels(IEnumerable<VehicleModelViewModel> models, string searchTerm);
-
-    // Paging Method
-    IEnumerable<VehicleModelViewModel> GetModelsForPage(IEnumerable<VehicleModelViewModel> models, int pageNumber, int pageSize);
+    Task<IEnumerable<VehicleModelViewModel>> GetModelsAsync(string searchTerm, string sortBy, bool ascending, int pageNumber, int pageSize);
 }
-
---
+++ VehicleManagementSystem.Service/Services/Vehicle/VehicleService.cs
@@ -0,0 +1,73 @@
+using AutoMapper;
+using VehicleManagementSystem.Service.Models;
+using VehicleManagementSystem.Service.Repositories;
+using VehicleManagementSystem.Service.ViewModels;
+
+namespace VehicleManagementSystem.Service.Services.Vehicle;
+
+public class VehicleService : IVehicleService
+{
+    private readonly IVehicleRepository _vehicleRepository;
+    private readonly IMapper _mapper;
+
+    public VehicleService(IVehicleRepository vehicleRepository, IMapper mapper)
+    {
+        _vehicleRepository = vehicleRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<IEnumerable<VehicleMakeViewModel>> GetAllMakesAsync()
+    {
+        IEnumerable<VehicleMake> makes = await _vehicleRepository.GetAllMakesAsync();
+        return _mapper.Map<IEnumerable<VehicleMakeViewModel>>(source: makes);
+    }
+
+    public async Task<Optional<VehicleMakeViewModel>> GetMakeByIdAsync(int id)
+    {
+        Optional<VehicleMake> makeOptional = await _vehicleRepository.GetMakeByIdAsync(id: id);
+        return makeOptional.HasValue ? Optional<VehicleMakeViewModel>.Of(_mapper.Map<VehicleMakeViewModel>(makeOptional.Value)) : Optional<VehicleMakeViewModel>.Empty();
+    }
+
+    public async Task AddMakeAsync(VehicleMakeViewModel makeViewModel)
+    {
+        VehicleMake make = _mapper.Map<VehicleMake>(makeViewModel);
+        await _vehicleRepository.AddMakeAsync(make: make);
+    }
+
+    public async Task UpdateMakeAsync(VehicleMakeViewModel makeViewModel)
+    {
+        VehicleMake make = _mapper.Map<VehicleMake>(makeViewModel);
+        await _vehicleRepository.UpdateMakeAsync(make: make);
+    }
+
+    public async Task DeleteMakeAsync(int id)
+    {
+        await _vehicleRepository.DeleteMakeAsync(id: id);
+    }
+    
+    // Model methods
+    public async Task<IEnumerable<VehicleModelViewModel>> GetAllModelsAsync()
+    {
+        IEnumerable<VehicleModel> models = await _vehicleRepository.GetAllModelsAsync();
+        return _mapper.Map<IEnumerable<VehicleModelViewModel>>(models);
+    }
+
+    public async Task<Optional<VehicleModelViewModel>> GetModelByIdAsync(int id)
+    {
+        Optional<VehicleModel> modelOptional = await _vehicleRepository.GetModelByIdAsync(id: id);
+        return modelOptional.HasValue ? Optional<VehicleModelViewModel>.Of(_mapper.Map<VehicleModelViewModel>(modelOptional.Value)) : Optional<VehicleModelViewModel>.Empty();
+
+    }
+
+    public async Task<IEnumerable<VehicleModelViewModel>> GetModelsByMakeIdAsync(int makeId)
+    {
+        IEnumerable<VehicleModel> models = await _vehicleRepository.GetModelsByMakeIdAsync(makeId: makeId);
+        return _mapper.Map<IEnumerable<VehicleModelViewModel>>(models);
+    }
+
+    public async Task<IEnumerable<VehicleModelViewModel>> GetModelsAsync(string searchTerm, string sortBy, bool ascending, int pageNumber, int pageSize)
+    {
+        IEnumerable<VehicleModel> models = await _vehicleRepository.GetModelsAsync(searchTerm: searchTerm, sortyBy: sortBy,  ascending: ascending, pageNumber: pageNumber, pageSize: pageSize);
+        return _mapper.Map<IEnumerable<VehicleModelViewModel>>(models);
+    }
+}
