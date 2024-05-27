97c836ee20127d66c7c201b13f985acd1e076aac
Marko Perica <marko.perica2@gmail.com>
2024-05-27 15:40:59 +0200

    Add Optional class

---
+++ VehicleManagementSystem.Service/Optional.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel;
+
+namespace VehicleManagementSystem.Service;
+
+public readonly struct Optional<T>
+{
+    private readonly T _value;
+
+    private Optional(T value)
+    {
+        HasValue = true;
+        _value = value;
+    }
+
+    public static Optional<T> Of(T value) => new Optional<T>(value: value);
+    
+    // Instance of this struct with all its fields set to their default values.
+    public static Optional<T> Empty() => default;
+
+    public bool HasValue { get; }
+
+    public T Value
+    {
+        get
+        {
+            if (!HasValue)
+            {
+                throw new InvalidOperationException(message: "Optional<T> does not have a value.");
+            }
+
+            return _value;
+        }
+    }
+    
+    public T OrElse(T defaultValue) => HasValue ?_value : defaultValue;
+}
