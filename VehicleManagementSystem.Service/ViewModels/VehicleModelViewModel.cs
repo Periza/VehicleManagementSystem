9afd55009b087ceacb3047f1241bf29822fa7300
Marko Perica <marko.perica2@gmail.com>
2024-05-29 18:22:53 +0200

    Add data annotations to VehicleModelViewModel

--- VehicleManagementSystem.Service/ViewModels/VehicleModelViewModel.cs
+++ VehicleManagementSystem.Service/ViewModels/VehicleModelViewModel.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using VehicleManagementSystem.Service.Models;
 
 namespace VehicleManagementSystem.Service.ViewModels;
@@ -6,7 +7,12 @@ public class VehicleModelViewModel
 {
     public int Id { get; set; }
     public int MakeId { get; set; }
+    
+    [Required]
+    [StringLength(maximumLength: 100, ErrorMessage = "Name cannot be longer than 100 characters.")]
     public string Name { get; set; } = string.Empty;
+    [Required]
+    [StringLength(maximumLength: 100, ErrorMessage = "Name cannot be longer than 100 characters.")]
     public string Abrv { get; set; } = string.Empty;
-    public VehicleMake Make { get; set;  } 
+    public VehicleMake Make { get; set; } 
 }
