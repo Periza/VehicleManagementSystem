7f18853c26ec13043a67ca4d61028870e015fb44
Marko Perica <marko.perica2@gmail.com>
2024-05-29 10:14:36 +0200

    Add Upsert to controller

--- VehicleManagementSystem/Areas/Admin/Controllers/VehicleMakeController.cs
+++ VehicleManagementSystem/Areas/Admin/Controllers/VehicleMakeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VehicleManagementSystem.Service;
 using VehicleManagementSystem.Service.Services.Vehicle;
 using VehicleManagementSystem.Service.ViewModels;
 
@@ -16,11 +18,57 @@ public class VehicleMakeController : Controller
         _vehicleService = vehicleService;
     }
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string sortOrder)
     {
         IEnumerable<VehicleMakeViewModel> models = await _vehicleService.GetAllMakesAsync();
         return View(models);
     }
+    
+    
+    public async Task<IActionResult> Upsert(int? id = null) // Update insert
+    {
+        // If id is null or zero create a new VehicleMake view model and pass it to the view
+        if (id is null or 0)
+        {
+            VehicleMakeViewModel vmViewModel = new();
+            return View(vmViewModel);
+        }
+
+        Optional<VehicleMakeViewModel> vmOptional = await _vehicleService.GetMakeByIdAsync(id.Value);
+        if (!vmOptional.HasValue)
+            return NotFound();
+
+        return View(vmOptional.Value);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Upsert(VehicleMakeViewModel vmViewModel)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (vmViewModel.Id is 0)
+                {
+                    TempData["success"] = "Vehicle Make added successfully!";
+                    await _vehicleService.AddMakeAsync(vmViewModel);
+                }
+                else
+                {
+                    TempData["success"] = "Vehicle Make updated successfully!";
+                    await _vehicleService.UpdateMakeAsync(vmViewModel);
+                }
+            }
+        }
+        catch
+        {
+            TempData["error"] = $"Error while {(vmViewModel.Id is 0 ? "creating" : "updating")} vehicle make";
+        }
+
+        return RedirectToAction(actionName: nameof(Index));
+    }
+    
+    
 
     public async Task<IActionResult> Delete(int id)
     {
@@ -34,7 +82,7 @@ public class VehicleMakeController : Controller
             TempData["error"] = "Error while deleting vehicle make";
         }
 
-        return RedirectToAction(nameof(Index));
+        return RedirectToAction(actionName: nameof(Index));
     }
     
 }
